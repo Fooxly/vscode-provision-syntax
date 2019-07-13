77b6138cb3771c9305b03a31d4657df8a29fd882
Luc Hermkens <hermkensluc@gmail.com>
2019-07-13 17:37:06 +0200

    more optimisations

--- src/syntax/Syntaxing.ts
+++ src/syntax/Syntaxing.ts
@@ -6,9 +6,9 @@ export default class Syntaxing extends Provision {
   private styling: any = {}
 
   public onUpdate(data: any) {
-    if(!window.activeTextEditor) return
-    let syntax = this.settings.get<any>('syntax', [])
-    let groups: any = {}
+    if (!window.activeTextEditor) return
+    const syntax = this.settings.get<any>('syntax', [])
+    const groups: any = {}
 
     for(let s of syntax) {
       groups[s.keyword] = {
@@ -17,20 +17,20 @@ export default class Syntaxing extends Provision {
       }
     }
 
-    for(let group of data) {
+    for (let group of data) {
       for(let i of group.items) {
         let s = syntax.find((e: any) => { return e.keyword === i.keyword })
         if(!s || !groups[s.keyword]) continue
         groups[s.keyword].items.push(i.range)
       }
     }
-    for(let g in groups) {
+    for (let g in groups) {
       window.activeTextEditor.setDecorations(groups[g].settings, groups[g].items)
     }
   }
 
   private getStyling(syntax: any): any {
-    if(this.styling[syntax.keyword]) return this.styling[syntax.keyword]
+    if (this.styling[syntax.keyword]) return this.styling[syntax.keyword]
     return this.styling[syntax.keyword] = window.createTextEditorDecorationType({
       backgroundColor: syntax.backgroundColor,
       isWholeLine: syntax.highlight === 'line',
@@ -42,21 +42,10 @@ export default class Syntaxing extends Provision {
 
   private getRulerLane(lane: string): OverviewRulerLane | undefined {
     switch(lane) {
-      case 'left': {
-        return OverviewRulerLane.Left
-      }
-      case 'right': {
-        return OverviewRulerLane.Right
-      }
-      case 'center': {
-        return OverviewRulerLane.Center
-      }
-      case 'full': {
-        return OverviewRulerLane.Full
-      }
-      default: {
-        return
-      }
+      case 'left': return OverviewRulerLane.Left
+      case 'right': return OverviewRulerLane.Right
+      case 'center': return OverviewRulerLane.Center
+      case 'full': return OverviewRulerLane.Full
     }
   }
 
